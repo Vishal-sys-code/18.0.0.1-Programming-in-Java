ec281c07a7d3c25fad00cf0bf0e902d3cfebe4a8
Vishal Pandey <bishalpandey2001@gmail.com>
2022-07-02 16:43:53 +0530

    Added ClassPath and Programming Updated

---
+++ Java Basics/Programming/Solutions/Number Questions/Reverse_Number.java
@@ -0,0 +1,8 @@
+// package Programming.Solutions.Number Questions;
+import java.util.Scanner;
+class Reverse_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
